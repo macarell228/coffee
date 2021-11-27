e7dd3dacf905feed2eeeca4001921b5e78dfdc8f
macarell228 <egubareva06@mail.ru>
2021-11-27 16:06:36 +0300

    homework2

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from PyQt5.Qt import *
 from PyQt5.uic import loadUi
 
 
+class ReadOnlyDelegate(QStyledItemDelegate):
+    def createEditor(self, parent, option, index):
+        return
+
+
 class Coffee(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -11,8 +16,13 @@ class Coffee(QMainWindow):
 
         self.connection = sqlite3.connect('coffee.sqlite')
         self.cur = self.connection.cursor()
+        self.form = addEditCoffeeForm(self.connection)
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.tableWidget.verticalHeader().setVisible(False)
+        [self.tableWidget.setItemDelegateForColumn(i, ReadOnlyDelegate(self.tableWidget)) for i in range(7)]
         self.paste_data()
+
+        self.edit_or_add_coffee.triggered.connect(lambda x: self.form.show())
 
     def paste_data(self):
         for i in range(7):
@@ -35,6 +45,68 @@ class Coffee(QMainWindow):
                 item = QTableWidgetItem("{}".format(results[i][j]))
                 item.setTextAlignment(Qt.AlignHCenter)
                 self.tableWidget.setItem(i, j, item)
+
+
+class addEditCoffeeForm(QWidget):
+    def __init__(self, connection):
+        super().__init__()
+        loadUi('addEditCoffeeForm.ui', self)
+
+        self.titles = ['id', 'gradeOfCoffee', 'roastingDegree', 'coffeeType',
+                       'tasteDescription', 'price', 'packageVolume']
+        self.add_widgets = {self.coffeeType_textEdit, self.gradeOfCoffee_textEdit,
+                            self.roastingDegree_textEdit,
+                            self.taste_DescriptiontextEdit,
+                            self.price_textEdit, self.volume_textEdit}
+
+        self.connection = connection
+        self.cursor = self.connection.cursor()
+
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.setItemDelegateForColumn(0, ReadOnlyDelegate(self.tableWidget))
+        self.paste_data()
+
+        self.update_radioButton.toggled.connect(
+            lambda x: self.tableWidget.setEnabled(self.update_radioButton.isChecked()))
+        self.add_radioButton.toggled.connect(
+            lambda x: [widget.setEnabled(self.add_radioButton.isChecked()) for widget in
+                       self.add_widgets] and self.add_button.setEnabled(self.add_radioButton.isChecked()))
+        self.add_button.clicked.connect(self.add_row)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+
+    def change_db(self, query, params=None):
+        self.cursor.execute(query, (*params,)) if params else self.cursor.execute(query)
+        self.connection.commit()
+
+    def add_row(self):
+        self.change_db("INSERT INTO InfoAboutCoffee VALUES (NULL, ?, ?, ?, ?, ?, ?)",
+                       [widget.toPlainText() for widget in self.add_widgets])
+        [widget.clear() for widget in self.add_widgets]
+        self.paste_data()
+
+    def paste_data(self):
+        for i in range(7):
+            self.tableWidget.insertColumn(i)
+        self.tableWidget.setHorizontalHeaderLabels(self.titles)
+
+        results = self.cursor.execute("""SELECT * FROM InfoAboutCoffee""").fetchall()
+        rows = len(results)
+        columns = len(results[0])
+        self.tab(columns, rows, results)
+
+    def tab(self, columns, rows, results):
+        self.tableWidget.setRowCount(rows)
+        self.tableWidget.setColumnCount(columns)
+
+        for i in range(rows):
+            for j in range(columns):
+                item = QTableWidgetItem("{}".format(results[i][j]))
+                item.setTextAlignment(Qt.AlignHCenter)
+                self.tableWidget.setItem(i, j, item)
+
+    def item_changed(self, item):
+        return f"UPDATE InfoAboutCoffee SET {self.titles[item.column()]}=" + \
+               f"{item.text()} WHERE id={self.tableWidget.item(item.row(), 0).text()}"
 
 
 if __name__ == '__main__':
